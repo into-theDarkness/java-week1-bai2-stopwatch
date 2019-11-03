e935b26e56f3bb516acaaafc98c2322ac9e69195
into-theDarkness <zzpeterpan0510@gmail.com>
2019-11-03 11:47:54 +0700

    stopwatch

---
+++ src/StopWatch.java
@@ -0,0 +1,47 @@
+import java.util.Date;
+public class StopWatch {
+    private Date startTime;
+    private Date endTime;
+    public Date getStartTime(){
+        return  this.startTime;
+    }
+    public Date getEndTime(){
+        return this.endTime;
+    }
+    public StopWatch(){
+        this.startTime = new Date();
+    }
+    public void start(){
+        this.startTime = new Date();
+    }
+    public void stop(){
+        this.endTime = new Date();
+    }
+    public long getElapsedTime(){
+        Date endtime = this.getEndTime();
+        Date starttime = this.getStartTime();
+        return endtime.get  Time() - starttime.getTime();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = new int [100000];
+        for(int i =0; i < arr.length; i++){
+            arr[i] = (int) (Math.random()*100000);
+        }
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        for (int i = 0; i < arr.length; i++){
+            for (int j = i+1;j<arr.length;j++){
+                if (arr[i] < arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        stopWatch.stop();
+        System.out.println(stopWatch.getElapsedTime());
+
+    }
+
+}
